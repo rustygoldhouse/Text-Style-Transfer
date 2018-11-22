c5b6334f360b530eee20e6cae7bbd3dbc999100b
rustygoldhouse <35602065+rustygoldhouse@users.noreply.github.com>
2018-11-22 03:46:24 -0500

    r i

--- generate_text.py
+++ generate_text.py
@@ -9,29 +9,8 @@ to generate text in the style of training data.
 
 '''
 
-
-
-
-
-
-from keras.models import load_model
-import time
-import numpy as np
-
-file=open('kafka.txt')
-string=file.read()
-words=string.split()
-
-sentences=[words[i:i+5] for i in range(25000)]
-targets=[words[i+5] for i in range(25000)]
-
-
-vocab=list(set(words))
-
-words_to_code=dict((i,j) for j,i in enumerate(vocab))
-code_to_words=dict((i,j) for i,j in enumerate(vocab))
-
 def generate_test():
+    import time
     sentence='Our first principle was: pack and contagion, the contagion of the pack, such is the path becoming-animal takes. But a second principle seemed to tell us the opposite: wherever there is multipli- city, you will also find an exceptional individual, and it is with that individ- ual that an alliance must be made in order to become-animal. There may be no such thing as a lone wolf, but there is a leader of the pack, a master of the pack, or else the old deposed head of the pack now living alone, there is the Loner, and there is the Demon.'
     sentence=sentence.split()
     sentence=[words_to_code[w] for w in sentence]
@@ -51,5 +30,47 @@ def generate_test():
         sentence=sentence.reshape(1,5)
         time.sleep(1)
 
+
+
+
+"""
+from keras.models import load_model
+import time
+import numpy as np
+
+file=open(u'kafka.txt', 'r', encoding='utf-8')
+string=file.read()
+words=string.split()
+
+sentences=[words[i:i+5] for i in range(25000)]
+targets=[words[i+5] for i in range(25000)]
+
+
+vocab=list(set(words))
+
+words_to_code=dict((i,j) for j,i in enumerate(vocab))
+code_to_words=dict((i,j) for i,j in enumerate(vocab))
+
+def generate_test():
+    sentence=str(u'Our first principle was: pack and contagion, the contagion of the pack, such is the path becoming-animal takes. But a second principle seemed to tell us the opposite: wherever there is multipli- city, you will also find an exceptional individual, and it is with that individ- ual that an alliance must be made in order to become-animal. There may be no such thing as a lone wolf, but there is a leader of the pack, a master of the pack, or else the old deposed head of the pack now living alone, there is the Loner, and there is the Demon.')
+    sentence=sentence.split()
+    sentence=[words_to_code[w] for w in sentence]
+    sentence=np.array(sentence)
+    sentence=sentence.reshape(1,5)
+    
+    model=load_model('newtextmodel.h5')
+    
+    while True:
+        x=np.argmax(model.predict(sentence))
+        print(code_to_words[x],end=' ')
+        sentence=sentence.flatten()
+        sentence=sentence.tolist()
+        sentence.append(x)
+        sentence=sentence[1:]
+        sentence=np.array(sentence)
+        sentence=sentence.reshape(1,5)
+        time.sleep(1)
+"""
+        
 if __name__ == '__main__':
     generate_test()
--- train.py
+++ train.py
@@ -7,9 +7,49 @@ And the model will start training on the given dataset.
 
 '''
 
+from keras.layers import Dense,LSTM,Embedding
+from keras.utils import to_categorical
+from keras.models import Sequential
+from sklearn.cross_validation import train_test_split
+import numpy as np
+from gensim.models import Word2Vec
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+file=open('kafka.txt')
+string=file.read()
+words=string.split()
+
+sentences=[words[i:i+5] for i in range(5000)]
+targets=[words[i+5] for i in range(5000)]
+
+
+vocab=list(set(words[:5005]))
+
+words_to_code=dict((i,j) for j,i in enumerate(vocab))
+code_to_words=dict((i,j) for i,j in enumerate(vocab))
+newtargets=[words_to_code[w] for w in targets]
+for i in range(5000):
+    for j in range(5):
+        sentences[i][j]=words_to_code[sentences[i][j]]
+sentences=np.array(sentences)
 
 
 
+newtargets=to_categorical(newtargets,num_classes=len(vocab))
+newtargets=np.array(newtargets)
+
+model=Sequential()
+model.add(Embedding(len(vocab),40,input_length=5))
+model.add(LSTM(40,dropout=0.3,input_shape=(5,40)))
+model.add(Dense(len(vocab),activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['acc'])
+
+X_train,X_test,Y_train,Y_test=train_test_split(sentences,newtargets)
+
+
+"""
 from keras.layers import Dense,LSTM,Embedding
 from keras.utils import to_categorical
 from keras.models import Sequential
@@ -68,3 +108,4 @@ model.save('newtextmodel.h5')
 
 plt.plot(history.history['loss'])
 plt.show()
+"""
